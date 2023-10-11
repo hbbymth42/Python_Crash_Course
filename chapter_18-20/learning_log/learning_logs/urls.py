940ab5d5f37ee97c810a5f562f9c4b647d185bef
hbbymth42 <hobbymath42@gmail.com>
2023-10-11 19:33:59 +1100

    Completed Building Additional Pages Section

--- chapter_18-20/learning_log/learning_logs/urls.py
+++ chapter_18-20/learning_log/learning_logs/urls.py
@@ -5,7 +5,11 @@ from django.urls import path
 from . import views
 
 app_name = 'learning_logs'
-urlpatterns = {
+urlpatterns = [
     # Home page
-    path('', views.index, name='index')
-}
+    path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
